b93201d184f66e49198c2b3e384a1ad719251c5c
Yushi Matsui <mypc@Yushis-MacBook-Air.local>
2023-12-22 14:21:26 +0900

    add image

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 6 // Number of posts per page
+	author: 'Livspect, Inc.', // Site author
+	title: 'Astroを体感したかっただけのブログ', // Site title.
+	description: 'プレスから所感までが方針の雑記ブログ', // Description to display in the meta tags
+	lang: 'ja_JP',
+	ogLocale: 'ja_JP',
+	shareMessage: 'この投稿を共有する', // Message to share a post on social media
+	paginationSize: 5 // Number of posts per page
 }
